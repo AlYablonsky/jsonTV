30e11722bdd32a072ddc2491a843121b9ffe38da
Al Yablonsky <ablonsky@austin.rr.com>
2017-03-06 20:07:40 -0600

    Add files via upload

--- twitchTV.js
+++ twitchTV.js
@@ -1,41 +1,39 @@
 
-// Array to store Twitch TV channels
-	
-var twitchTVchannel =["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "comster404", "brunofin","MeteorDev", "SYNTAG", "ExtremeModeration", "Dr4xell", "frinlet", "vihart"];	
-
 function getTwitchChannels ()  {
 	
+	
+	var twitchTVchannel =['ESL_SC2', 'OgamingSC2', 'cretetion', 'freecodecamp', 'storbeck', 'habathcx', 'RobotCaleb', 'noobs2ninjas', 'comster404', 'brunofin','MeteorDev', 'SYNTAG', 'ExtremeModeration', 'Dr4xell', 'frinlet', 'vihart'];	
 	
     //Obtain channel stream and status api call
     
 	var url1, url2;
-	for (var i = 0; i < twitchTVchannel.length; i++) {
-    	url1 = "https://wind-bow.gomix.me/twitch-api/streams/" + twitchTVchannel[i] + "?callback=?";
+	for (let i = 0; i < twitchTVchannel.length; i++) {
+    	url1 = 'https://wind-bow.gomix.me/twitch-api/streams/' + twitchTVchannel[i] + '?callback=?';
 		
 		// JSON callback url from streams - gets channel status - needs index protecting function because of the callback loop
 		
-		(function(i) {
+	
   		$.getJSON(url1, function(data1) {
 			
 			var game, status;
 			if (data1.stream === null) {
 				
-				game = "Offline";
-				status = "offline";
+				game = 'Offline';
+				status = 'offline';
 				
 			} else if ( data1.stream === undefined) {
 				
-				game = "No account";
-				status = "offline";
+				game = 'No account';
+				status = 'offline';
 				
 			} else {
 				
 				game = data1.stream.game;
-				status = "online";
+				status = 'online';
 			}
 		
 			// Obtain channel specific information from second JSON call
-			url2 = 	"https://wind-bow.gomix.me/twitch-api/channels/" + twitchTVchannel[i];
+			url2 = 	'https://wind-bow.gomix.me/twitch-api/channels/' + twitchTVchannel[i];
 			$.getJSON(url2, function(data1) {				
 		
 			var name, logo, urlx, action;
@@ -49,10 +47,9 @@ function getTwitchChannels ()  {
 					name = data1.display_name;
 				}
 				
-			
 				if (data1.logo === undefined) {
 					
-					logo = "http://www.polytopia.net/noLogo.jpg";
+					logo = 'http://www.polytopia.net/noLogo.jpg';
 					
 				}else {
 					
@@ -60,18 +57,18 @@ function getTwitchChannels ()  {
 				}
 					
 				
-				if (status === "online") {
+				if (status === 'online') {
 					
-					action = " - " + data1.status;
+					action = ' - ' + data1.status;
 				}
 				else {
 					
-					action = "";
+					action = '';
 				}
 
 				if (data1.url === undefined) {
 						
-					urlx = "#";
+					urlx = '#';
 					
 				}else {
 					
@@ -80,52 +77,48 @@ function getTwitchChannels ()  {
 								
 				var toggle;  // sends online and offline channels to a different class where they can be styled separately
 				
-				if (status === "online"){
-					toggle = "onLineFollowers";
+				if (status === 'online'){
+					toggle = 'onLineFollowers';
 				}
 				else {
-					toggle = "offLineFollowers";
+					toggle = 'offLineFollowers';
 				}
 		
 	
 	// Ptinting to HTML			
-	html ="<div class='row " + toggle + "'>" + "<div class='col-md-3' id='imageAlign'>" + "<img src='" +logo + "'id ='logo' width= '100px' height= '100px'>" + "</div>"+ "<div class='col-md-3'>" + "<a href=" + urlx + " target= '_blank'>" +  name + "</a></div>" + "<div class='col-md-6'>" + game + action + "</div></div>";	
+	html ='<div class="row ' + toggle + '">' + '<div class="col-md-3" id="imageAlign">' + '<img src="' +logo + '"id ="logo" width= "100px" height= "100px">' + '</div>'+ '<div class="col-md-3">' + '<a href=' + urlx + ' target= "_blank">' +  name + '</a></div>' + '<div class="col-md-6">' + game + action + '</div></div>';	
 				
-		       if (status === "online") {
+		       if (status === 'online') {
 				   
-					$(".followerInfo").prepend(html);
+					$('.followerInfo').prepend(html);
 			   }
 				else {
 					
-					$(".followerInfo").append(html);
-				}
-				});  // end of inner JSON
-			});  // end of outer first JSON 
-		})(i);  // added to "protector function"
-	}   // end of for loop
+					$('.followerInfo').append(html);
+              }
+		   });  
+       });   
+	}   
 }
 
 
 $(document).ready(function() {
 	
 
-	getTwitchChannels();  // Displays Channel results
+	getTwitchChannels(); 
 	
-	// Slow-Hide statement for on/offline channels
-	$("#showOnLineChannels").click(function(e){
+	$('#showOnLineChannels').click(function(e){
 		 $('.onLineFollowers').show();	
 		 $('.offLineFollowers').hide();	
 	}); 
-	$("#showOffLineChannels").click(function(e){
+	$('#showOffLineChannels').click(function(e){
 		$('.offLineFollowers').show();	
 		$('.onLineFollowers').hide();	
 	});
-	$("#showAllChannels").click(function(e){
+	$('#showAllChannels').click(function(e){
 		$('.onLineFollowers').show();
 		$('.offLineFollowers').show();	
 	});
 
-});  // End of Statement
-
-
+}); 
 
